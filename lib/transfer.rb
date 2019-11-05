57ccc0fbf12beda1bf4154dbbbde43d440d3f1cc
Christian Perez <perezchristian1012@gmail.com>
2019-11-05 18:14:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,8 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    elsif @status = "rejected"
+    else
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
